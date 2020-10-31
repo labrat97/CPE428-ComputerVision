0651212faf07725a9404d776ecb8adfb0ca3d2c7
James McRoberts <jamesmcroberts82997@gmail.com>
2020-10-30 20:04:19 -0700

    Began homework 3.

---
+++ Homework 3/hybrid.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a hybrid of images" +
+        "based off the features found in them.")
+    
